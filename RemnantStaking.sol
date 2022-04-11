ed96ee48cdf14deac4f7b0cfadb1fbc3b3fd2779
Kenny <ken.le@hotmail.com>
2022-04-11 16:42:09 -0400

    just a comment

--- RemnantStaking.sol
+++ RemnantStaking.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract RemnantStaking is Ownable {
+contract RemnantStaking is Ownable { // Just an exact duplicate of StakingVested.sol
 
     struct Deposit {
         uint256 tokenAmount;
